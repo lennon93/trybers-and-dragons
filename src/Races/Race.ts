f6e243e560f247a3d5c9793d8150309befb47f7a
Lennon <lennon_lxs@hotmail.com>
2023-06-06 00:27:14 -0300

    Cria a classe abstrata Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,23 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number; 
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+}
